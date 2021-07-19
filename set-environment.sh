da22e65040a47e3b107ae14498278d137eea496a
Niki <Niki@niki.info>
2021-07-19 08:59:15 -0400

    fix Docker metricks

--- set-environment.sh
+++ set-environment.sh
@@ -8,6 +8,17 @@ yum install -y python3 python3-pip && \
 pip3 install cryptography && \
 pip3 install ansible
 
-mkdir /apps && cd /apps
+# set folder structure
+project_dir="/apps"
 
+mkdir $project_dir && cd /project_dir
+
+# clone repo
 git clone https://github.com/N-Borisov/flask-project.git
+
+# replace ip for prometheus monitoring
+sed "s/localhost:9323/$ips:9323/" flask-project/prometheus.yml
+sed "s/localhost:9104/$ips:9104/" flask-project/prometheus.yml
+sed "s/localhost:9323/$ips:9323/" flask-project/daemon.json
+
+ansible-playbook setup-docker.yml
